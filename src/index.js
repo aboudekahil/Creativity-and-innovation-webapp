03d13cf1242076be483ed492e59e8d69a9d416f5
aboude <53794997+aboudekahil@users.noreply.github.com>
2023-04-12 12:46:29 +0300

    cdns turned into local files 😨

--- src/index.js
+++ src/index.js
@@ -2,16 +2,17 @@ import express from 'express';
 import path from 'path';
 import * as url from 'url';
 
-const __filename = url.fileURLToPath(import.meta.url);
 const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
-const frontend_dir = path.join(__dirname, '../frontend/');
+const basedir = path.join(__dirname, '..');
 
 const app = express();
 const port = process.env.PORT || 8080;
 
 app.get('/', (req, res) => {
-  res.sendFile('index.html', { root: frontend_dir });
+  res.sendFile(path.join(basedir, 'frontend', 'index.html'));
 });
 
 app.listen(port);
-console.log('Server started at http://localhost:' + port);
+console.log(`Server started at http://localhost:${port}`);
+
+app.use(express.static('frontend'));
